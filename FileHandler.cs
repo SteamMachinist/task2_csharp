ceb0035fec4fcdbc274cc2bcf5a64a0bbc6a208c
rando <roshchupkin.ya@gmail.com>
2023-03-18 01:41:51 +0300

    initial

---
+++ FileHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_csharp
+{
+    internal class FileHandler
+    {
+        public static string readFile(string path) 
+        { 
+            return File.ReadAllText(path); 
+        }
+
+        public static void writeFile(string path, string content)
+        {
+            File.WriteAllText(path, content);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 28.  Прочитать текстовый файл. На отдельной форме вывести тот же текст, но в котором все числа записаны словами
+ */
+
+namespace task2_csharp
+{
+    internal class Program
+    {
+        FileHandler handler = new FileHandler();
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
